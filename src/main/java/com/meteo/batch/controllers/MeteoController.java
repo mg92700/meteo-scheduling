2d86aeaea5c6bd58b573b949e5d9aeaff2d5a4e1
mg9700 <ghoraf.mouhsin@gmail.com>
2024-09-07 21:45:29 +0200

    add request by insee

--- src/main/java/com/meteo/batch/controllers/MeteoController.java
+++ src/main/java/com/meteo/batch/controllers/MeteoController.java
@@ -3,12 +3,11 @@ package com.meteo.batch.controllers;
 import com.meteo.batch.model.MeteoEntity;
 import com.meteo.batch.services.meteo.MeteoService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/meteo")
@@ -26,6 +25,15 @@ public class MeteoController {
     public @ResponseBody List<MeteoEntity> getAllMeteo() {
         log.info("Request all meteo");
         return meteoService.all();
+    }
+
+    @GetMapping(path ="/byInsee")
+    public @ResponseBody Optional<MeteoEntity> geMeteoByInsee(@RequestParam String location) {
+        log.info("Request all meteo");
+        return meteoService.all().stream()
+                .filter(c -> c.getInsee().equals(location))
+                .sorted(Comparator.comparing(MeteoEntity::getDatesaving).reversed())
+                .findFirst();
     }
 
 }
